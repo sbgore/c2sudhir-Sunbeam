689f44f9440c2daa65a1abaa176a407f67195eea
sudhir Gore <sbgore193@gmail.com>
2023-09-15 11:04:40 +0530

    Day2 reamining

---
+++ Day2/python5.py
@@ -0,0 +1,5 @@
+# 3)Replace single element ‘b’ in given list [’a’, ’b’, ’c’, ’d’, ’e’] with [1, 2, 3]
+word = ['a', 'b', 'b', 'd', 'e']
+index = word.index('b')
+word[index ]= [1, 2, 3]
+print(word)
