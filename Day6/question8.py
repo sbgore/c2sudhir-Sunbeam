8b23822728228124f4c7de3c955daff6c5894fd1
sudhir Gore <sbgore193@gmail.com>
2023-09-16 15:24:45 +0530

    question8.py

--- Day6/question8.py
+++ Day6/question8.py
@@ -3,3 +3,29 @@
 # the third index, etc., and the element in the last index moves to the first index.Input:-
 # [‘Sunday’,’Monday’,’Tuesday’,’Wednesday’]
 # Output:- [’Wednesday’, ‘Sunday’, ’Monday’, ’Tuesday’]
+# word1 = ['sunday', 'Monday', 'tuesday', 'wednesday']
+# print(len(word1))
+# first_element = word1[0]
+# for i in range(len(word1)):
+#     word1[i] = word1[i + 1]
+#
+# word1[-1] = first_element
+# print(word1)
+def rotate_list(arr):
+    if len(arr) <= 1:
+        return arr  # Nothing to rotate if the list has 0 or 1 element
+
+    first_element = arr[0]  # Store the first element
+    for i in range(len(arr) - 1):
+        arr[i] = arr[i + 1]  # Shift elements to the left
+
+    arr[-1] = first_element  # Set the last element to the first
+
+# Input list
+input_list = ['Sunday', 'Monday', 'Tuesday', 'Wednesday']
+
+# Rotate the list
+rotate_list(input_list)
+
+# Print the rotated list
+print(input_list)
